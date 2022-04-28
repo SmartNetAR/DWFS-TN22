583c8a65aa834679c1058ca4263848cd4fb87171
Leonardo Casales <leonardo@smartnet.com.ar>
2022-04-27 21:56:21 -0300

    repaso js, funciones, arrays, objetos

---
+++ repaso-js-01/arrays.js
@@ -0,0 +1,27 @@
+
+const persona = ["leo", 22, 2000, true]
+
+console.log('>>> persona:');
+console.log(persona);
+
+const nombresFrutas = ["ananá", "frutilla", "manzana"]
+const nombresFrutas2 = [
+    "ananá",
+    "frutilla",
+    "manzana",
+]
+
+const documento = {
+    tipo: "DNI",
+    numero: 12345678,
+    nacionalidad: {
+        pais: "argentina",
+        provincia: "buenos aires"
+    }
+}
+documento.tipo = "PASAPORTE";
+
+console.log(nombresFrutas[0])
+
+console.log(documento.numero)
+
---
+++ repaso-js-01/main.js
@@ -0,0 +1,9 @@
+function sumar(valor1, valor2) 
+{
+    const suma = valor1 + valor2;
+    
+    return suma;
+}
+
+console.log(sumar(44, 99))
+console.log(sumar(100, 99))
