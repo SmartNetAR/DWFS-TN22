d9c7fda17fc8ab20a6a7b7181a0a18192e50e451
Leonardo Casales <leonardo@smartnet.com.ar>
2022-06-22 20:49:43 -0300

    feat: ejs added

--- mvc/src/app.js
+++ mvc/src/app.js
@@ -1,13 +1,25 @@
+const path = require("path");
 const express = require("express");
 const mainRoutes = require("./routes/main");
 const productsRoutes = require("./routes/products");
+const productsRoutesApi = require("./routes/productsApi");
 
 const PORT = 3000;
 
 const app = express();
 
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname,  "./views"));
+
 app.use("/", mainRoutes);
+
+// rutas web que devuelven html
 app.use("/productos", productsRoutes);
+
+// rutas de api, que devuelven json
+app.use("/api/productos", productsRoutesApi);
 
 
 app.listen(PORT, ()=> console.log(`Escuchando en el puerto ${PORT}`));
