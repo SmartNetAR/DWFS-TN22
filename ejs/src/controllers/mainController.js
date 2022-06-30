86688e67c885f8ae37e7c9081c74522ccb0a7aa1
Leonardo Casales <leonardo@smartnet.com.ar>
2022-06-29 21:27:47 -0300

    ejercicio agregado

---
+++ ejs/src/controllers/mainController.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+const controller = {
+	index: (req, res) => {
+		// Do the magic
+		res.render("index")
+	},
+	search: (req, res) => {
+		// Do the magic
+	},
+};
+
+module.exports = controller;
