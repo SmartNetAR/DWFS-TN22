779f82f60f0952a93051233c80db22bfd3cf98be
Leonardo Casales <leonardo@smartnet.com.ar>
2022-09-19 21:33:15 -0300

    change function component to class component

--- react-01/proyecto/src/App.js
+++ react-01/proyecto/src/App.js
@@ -1,40 +1,61 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
 import './App.css';
 import Avatar from './perfil/components/Avatar';
 import Footer from './layout/footer/components/Footer';
 
-function App() {
-  const usuarios = [
-    {name: "lucas", isBot: true, following: true}, 
-    {name: "lucas", isBot: true, following: true}, 
-    {name: "juan", isBot: false, following: false}, 
-    {name: "martin", isBot: false, following: true}, 
-    {name: "cristian", isBot: true, following: true}, 
-  ];
+class App extends Component {
 
-  function handleOnToggleSeguir(event) {
-    console.log(event)
+  constructor() {
+    super();
+    this.state = {
+      usuarios: [
+        {id: 0, name: "marcelo", isBot: false, following: false}, 
+        {id: 1, name: "lucas", isBot: true, following: false}, 
+        {id: 2, name: "lucas", isBot: true, following: true}, 
+        {id: 3, name: "juan", isBot: false, following: false}, 
+        {id: 4, name: "martin", isBot: false, following: true}, 
+        {id: 5, name: "cristian", isBot: true, following: true}, 
+      ]
+    }
   }
 
-  return (
-    <>
-      <div className="App">
-        <header className="App-header">
-          {/* <Avatar/> */}
-          {usuarios.map((usr, idx)=> (
-            <Avatar
-              nombre={usr.name} 
-              esRobot={usr.isBot}
-              esSeguido={usr.following}
-              key={usr.name + idx}
-              onToggleSeguir={handleOnToggleSeguir}
-            />))}
-          {/* {usuarios.map((usr)=> <p>{usr}</p>)} */}
-        </header>
-      </div>
-      <Footer/>
-    </>
-  );
+  handleOnToggleSeguir(usuario) {
+    return () => {
+      let followingNewValue = !usuario.following;
+
+      const newUsersArray = Array.from(this.state.usuarios);
+      newUsersArray[usuario.id].following = followingNewValue;
+
+      this.setState({
+        usuarios: newUsersArray
+      })
+
+    }
+  }
+
+  render() {
+
+    return (
+      <>
+        <div className="App">
+          <header className="App-header">
+            {/* <Avatar/> */}
+            {this.state.usuarios.map((usr, idx)=> (
+              <Avatar
+                nombre={usr.name} 
+                esRobot={usr.isBot}
+                esSeguido={usr.following}
+                key={usr.name + idx}
+                onToggleSeguir={this.handleOnToggleSeguir(usr)}
+              />))}
+            {/* {usuarios.map((usr)=> <p>{usr}</p>)} */}
+          </header>
+        </div>
+        <Footer/>
+      </>
+    );
+
+  }
 }
 
 export default App;
